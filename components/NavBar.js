7276b65140714793345eaf125b7802a13898588a
YooHyeok <yjk7454@naver.com>
2024-02-27 16:26:48 +0900

    refactor: NavBar - Style JSX문법 적용

--- components/NavBar.js
+++ components/NavBar.js
@@ -4,13 +4,29 @@ import styles from "./NavBar.module.css"
 export default function NavBar() {
   const router = useRouter();
 
-  return <nav >
+  return <nav>
     <Link href="/">
+      <a className={router.pathname === "/" ? "active" : ""}>Home</a>
+    </Link>
+    <Link href="/about">
+      <a className={router.pathname === "/about" ? "active" : ""}>about</a>
+    </Link>
+    {/* <Link href="/">
       <a className={`${styles.link} ${router.pathname==="/" ? styles.active : ""}`}>Home</a>
     </Link>
     <Link href="/about">
       <a className={[styles.link, router.pathname==="/about" ? styles.active : ""].join(" ")}>about</a>
-    </Link>
-
+    </Link> */}
+    <style jsx>{`
+        nav {
+          background-color: tomato;
+        }
+        a{
+          text-decoration: none;
+        }
+        .active {
+          color: yellow;
+        }
+    `}</style>
   </nav>
 }
