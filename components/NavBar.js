523a59875c695af1e5c666dace564eeb65cb31bf
YooHyeok <yjk7454@naver.com>
2024-02-27 17:34:23 +0900

    feat: Custom App 컴포넌트  _app.tsx 추가 및 NavBar, global.css 적용

--- components/NavBar.js
+++ components/NavBar.js
@@ -18,14 +18,11 @@ export default function NavBar() {
       <a className={[styles.link, router.pathname==="/about" ? styles.active : ""].join(" ")}>about</a>
     </Link> */}
     <style jsx>{`
-        nav {
-          background-color: tomato;
-        }
         a{
           text-decoration: none;
         }
         .active {
-          color: yellow;
+          color: tomato;
         }
     `}</style>
   </nav>
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,5 @@
-import NavBar from "../components/NavBar";
-
 export default function Home() {
   return <div>
-    <NavBar/>
     <h1>hi</h1>
   </div>
 }
