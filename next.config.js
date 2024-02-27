450cd4b188ddf3fc21e1ba6e5d13c4822cd2a92b
YooHyeok <yjk7454@naver.com>
2024-02-28 01:46:50 +0900

    feat: next.config.js - redirects, rewrites 설정 및 API Key .env 환경변수 지정

--- next.config.js
+++ next.config.js
@@ -1,7 +1,33 @@
 /** @type {import('next').NextConfig} */
+
+const API_KEY = process.env.API_KEY
+
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  async redirects() {
+    return [
+      {
+        source: "/naver", /* 사용자의 이동 경로 */
+        destination: 'https://www.naver.com', /* redirect될 경로 */
+        permanent: false, /* redirection이 영구적인지 여부에 따라 브라우저 검색엔진 정보기억 여부결정 */
+      },
+      {
+        source: "/old-blog/:path*", /* 예전 블로그 */
+        destination: '/new-blog/:path*', /* 블로그 이전 주소 */
+        permanent: false,
+      }
+    ]
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/api/movies",
+        destination: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
+      },
+    ]
+  }
 }
 
 module.exports = nextConfig
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,27 @@
+import { useEffect, useState } from "react";
 import Seo from "../components/Seo"
 
 export default function Home() {
+  const [movies, setMovies] = useState();
+
+  const getMovies = async () => {
+    const response = await fetch("/api/movies")
+    const {results} = await response.json();
+    setMovies(results);
+  }
+
+  useEffect(()=> {
+    getMovies()
+  }, [])
+
   return <div>
     <Seo title={"Home"}/>
-    <h1>hi</h1>
+    {!movies && <h4>Loading...</h4>}
+    {movies?.map(movie=> (
+      <div key={movie.id}>
+        <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
+        <h4>{movie.original_title}</h4>
+      </div>
+    ))}
   </div>
 }
