0abd2ee7951cd30f671ba66992fa2e698d085f0a
webdevyoo <yjk7454@naver.com>
2024-02-29 11:17:22 +0900

    feat: index.js getServerSideProps() - server side render 방식 적용

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
 import { useEffect, useState } from "react";
 import Seo from "../components/Seo"
 
-export default function Home() {
-  const [movies, setMovies] = useState();
+export default function Home({results}) {
+/*   const [movies, setMovies] = useState();
 
   const getMovies = async () => {
     const response = await fetch("/api/movies")
@@ -12,13 +12,14 @@ export default function Home() {
 
   useEffect(()=> {
     getMovies()
-  }, [])
-
+  }, []) */
+  console.log(results)
   return (
     <div className="container">
       <Seo title="Home" />
-      {!movies && <h4>Loading...</h4>}
-      {movies?.map((movie) => (
+      {/* {!movies && <h4>Loading...</h4>}
+      {movies?.map((movie) => ( */}
+      {results?.map((movie)=> (
         <div className="movie" key={movie.id}>
           <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
           <h4>{movie.original_title}</h4>
@@ -47,4 +48,16 @@ export default function Home() {
       `}</style>
     </div>
   )
+}
+
+export async function getServerSideProps() {
+    const response = await fetch("http://localhost:3000/api/movies")
+    const {results} = await response.json();
+    // const {results} = await (await fetch("https://localhost:3000/api/movies")).json()
+    console.log(results)
+    return {
+      props: {
+        results,
+      },
+    }
 }
